492590f79cda398bf4b9125420f3ab3d03e4970b
Bhat, Ramyashree <rbhat@dovercorp.com>
2018-10-31 14:22:06 +0530

    Committing

--- FrameworkTests/BrowserTests.cs
+++ FrameworkTests/BrowserTests.cs
@@ -4,28 +4,28 @@ using OpenQA.Selenium;
 
 namespace FrameworkTests
 {
-    //[TestClass]
-    //public class BrowserTests
-    //{
-    //    [TestMethod]
-    //    public void GivenValidElement_WhenElementIsVisible_ElementIsDisplayedFindsElement()
-    //    {
-    //        Browser.GoTo("data:text/html,<span id =\"hello\">Hello</span>",false);
-    //        Assert.IsTrue(Browser.ElementIsDisplayed(By.Id("hello")), "ElementIsDisplayed Method is supposed to find the element");
-    //    }
+    [TestClass]
+    public class BrowserTests
+    {
+        [TestMethod]
+        public void GivenValidElement_WhenElementIsVisible_ElementIsDisplayedFindsElement()
+        {
+            Browser.GoTo("data:text/html,<span id =\"hello\">Hello</span>", false);
+            Assert.IsTrue(Browser.ElementIsDisplayed(By.Id("hello")), "ElementIsDisplayed Method is supposed to find the element");
+        }
 
-    //    [TestMethod]
-    //    public void GivenValidElement_WhenElementIshidden_ElementIsDisplayedDoesNotFindElement()
-    //    {
-    //        Browser.GoTo("data:text/html,<span id =\"hello\" style=\"position:absolute;left:-9000px;width=0;overflow:hidden\">Hello</span>", false);
-    //        Assert.IsFalse(Browser.ElementIsDisplayed(By.Id("hello")), "ElementIsDisplayed found an element invalidly");
-    //    }
+        [TestMethod]
+        public void GivenValidElement_WhenElementIshidden_ElementIsDisplayedDoesNotFindElement()
+        {
+            Browser.GoTo("data:text/html,<span id =\"hello\" style=\"position:absolute;left:-9000px;width=0;overflow:hidden\">Hello</span>", false);
+            Assert.IsFalse(Browser.ElementIsDisplayed(By.Id("hello")), "ElementIsDisplayed found an element invalidly");
+        }
 
-    //    [TestMethod]
-    //    public void GivenValidElement_WhenElementDoesNotHaveID_ElementIsDisplayedFindsElement()
-    //    {
-    //        Browser.GoTo("data:text/html,hello", false);
-    //        Assert.IsFalse(Browser.ElementIsDisplayed(By.Id("hello")), "ElementIsDisplayed Method found an element that should not have been found");
-    //    }
-    //}
+        [TestMethod]
+        public void GivenValidElement_WhenElementDoesNotHaveID_ElementIsDisplayedFindsElement()
+        {
+            Browser.GoTo("data:text/html,hello", false);
+            Assert.IsFalse(Browser.ElementIsDisplayed(By.Id("hello")), "ElementIsDisplayed Method found an element that should not have been found");
+        }
+    }
 }
